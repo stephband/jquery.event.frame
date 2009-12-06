c9e7451fd245c6e9d598523906e24a738f69d41f
stephband <stephband@stephband.local>
2009-12-06 18:42:46 +0100

    Added support for namespaced frame events. Each namespace runs its own timer.

--- js/jquery.event.frame.js
+++ js/jquery.event.frame.js
@@ -1,5 +1,5 @@
 // jquery.events.frame.js
-// 1.0
+// 1.1
 // Stephen Band
 // 
 // Project home
@@ -8,60 +8,126 @@
 (function(jQuery){
 
 var undefined,
-    timer = {
-        frameDur: 40,
-        start: function(elem, fd){
-            if ( !this.clock ) {
-                // Init the clock
-                this.handler = jQuery.event.special.frame.handler;
-                this.frameCount = 0;
-                this.array = [elem];
-                this.clockFirst = setTimeout(this.trigger, 0);
-                this.clock = setInterval(this.trigger, fd || this.frameDur);
-            }
-            else {
-                // Add element to array
-                this.array.push(elem);
-            }
-        },
-        stop: function(elem){
-            var array = this.array,
-                l = array.length;
-            
-            // Remove element from list
-            while (l--) {
-                if (array[l] === elem) {
-                    array.splice(l, 1);
-                    break;
-                }
-            }
-            // Stop clock when list is empty
-            if ( array.length === 0 ) {
-                clearInterval( this.clock );
-                this.clockFirst = null;
-                this.clock = null;
-            }
-        },
-        trigger: function(){
-            var fn = timer.handler,
-                event = jQuery.Event("frame"),
-                array = timer.array,
-                l = array.length;
-            
-            // Update event and use it to call handler
-            event.frameCount = timer.frameCount++;
-            while (l--) { fn.call(array[l], event); }
-        }
+    timers = {};
+
+// Timer constructor
+// fn callback - function to call on each frame with 'this' set to the timer object
+// fd frameDuration - milliseconds
+
+function Timer( fn, fd ) {
+    var self = this,
+        clock;
+    
+    function update(){
+        self.frameCount++;
+        fn.call(self);
+    }
+    
+    this.frameDuration = fd || 25 ;
+    this.frameCount = 0 ;
+    this.start = function(){
+        update();
+        clock = setInterval(update, this.frameDuration);
     };
+    this.stop = function(){
+        clearInterval( clock );
+        clock = null;
+    };
+}
+
+// callHandler() is called a timer object
+// 'this' is the timer object
+
+function callHandler(){
+    
+    var fn = jQuery.event.special.frame.handler,
+        event = jQuery.Event("frame"),
+        array = this.array,
+        l = array.length;
+    
+    // Give event object properties
+    event.frameCount = this.frameCount;
+    
+    // Call handler on each elem in array
+    while (l--) {
+        fn.call(array[l], event);
+    }
+}
+
+// spork() is called for each bound namespace, and
+// decides whether or not to launch a new timer
+
+function spork( elem, name, fd ) {
+    // If timer is already running, add this to the list of elems it calls
+    if ( timers[name] ) {
+        timers[name].array.push(elem);
+    }
+    // Otherwise create a new timer with this in the list, and set it going
+    else {
+        timers[name] = new Timer( callHandler, fd );
+        
+        // Attach elem array to timer object
+        timers[name].array = [elem];
+        
+        // Queue timer to start as soon as this thread has finished -
+        // event handler is not yet ready to be called because setup is not complete
+        var t = setTimeout(function(){
+            timers[name].start();
+            clearTimeout(t);
+            t = null;
+        }, 0);
+    }
+}
+
+function unspork( elem, name ) {
+    var array = timers[name].array,
+        l = array.length;
+    
+    // Remove element from list
+    while (l--) {
+        if (array[l] === elem) {
+            array.splice(l, 1);
+            break;
+        }
+    }
+    
+    // Stop and remove timer when no elems left
+    if (array.length === 0) {
+        timers[name].stop();
+        timers[name] = undefined;
+    }
+}
 
 jQuery.event.special.frame = {
-    setup: function(data) {
-        timer.start( this, (data && data.frameDuration) );
-        //return false;
+    setup: function(data, namespaces) {
+        var l = namespaces.length;
+        
+        // No namespaces, so spork master timer
+        if (!l) {
+            spork( this, 0, data && data.frameDuration );
+            return;
+        }
+        
+        // Namespaces, so spork new timer for each namespace
+        while (l--) {
+            spork( this, namespaces[l], data && data.frameDuration );
+        }
     },
-    teardown: function() {
-        timer.stop( this );
-        //return false;
+    teardown: function(namespaces) {
+        var l = namespaces.length;
+        
+        // No namespaces, so unspork from all timers
+        if (!l) {
+            for (var name in timers ) {
+                unspork( this, name );
+            }
+            return;
+        }
+        
+        // Namespaces, so unspork timer for each name
+        while (l--) {
+            unspork( this, namespaces[l] );
+        }
     },
     handler: function(event){
         // let jQuery handle the calling of event handlers
