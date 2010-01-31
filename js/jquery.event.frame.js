5d8952d9af89fd676a2efd4ccbaf0393fae06021
stephband <stephband@stephband.local>
2010-01-31 23:18:24 +0100

    Adds lite, no namespace version of 1.1, and compiled version

--- js/jquery.event.frame.js
+++ js/jquery.event.frame.js
@@ -13,9 +13,10 @@
 var undefined,
     timers = {};
 
+
 // Timer constructor
-// fn callback - function to call on each frame with 'this' set to the timer object
-// fd frameDuration - milliseconds
+// fn - callback to call on each frame, with context set to the timer object
+// fd - frame duration in milliseconds
 
 function Timer( fn, fd ) {
     var self = this,
@@ -38,13 +39,13 @@ function Timer( fn, fd ) {
     };
 }
 
-// callHandler() is called a timer object
-// 'this' is the timer object
+
+// callHandler() is the callback given to the timer object
+// context is the timer object
 
 function callHandler(){
-    
     var fn = jQuery.event.special.frame.handler,
-        event = jQuery.Event("frame"),
+        event = jQuery.Event("frame." + this.name),
         array = this.array,
         l = array.length;
     
@@ -63,14 +64,26 @@ function callHandler(){
 function spork( elem, name, fd ) {
     // If timer is already running, add this to the list of elems it calls
     if ( timers[name] ) {
+        console.log('"' + name + '" already running. Adding ' + elem + ' to list.');
+        
+        // Is elem already in the list?
+        var l = timers[name].array.length;
+        while (l--) {
+            if (timers[name].array[l] === elem) { return; }
+        }
+        
+        // So put it in, then.
         timers[name].array.push(elem);
     }
     // Otherwise create a new timer with this in the list, and set it going
     else {
+        console.log('"' + name + '" created with ' + elem + ' in list.');
+        
         timers[name] = new Timer( callHandler, fd );
         
         // Attach elem array to timer object
         timers[name].array = [elem];
+        timers[name].name = name;
         
         // Queue timer to start as soon as this thread has finished -
         // event handler is not yet ready to be called because setup is not complete
@@ -80,6 +93,8 @@ function spork( elem, name, fd ) {
             t = null;
         }, 0);
     }
+    
+    console.log(timers);
 }
 
 function unspork( elem, name ) {
@@ -96,13 +111,21 @@ function unspork( elem, name ) {
     
     // Stop and remove timer when no elems left
     if (array.length === 0) {
+        console.log('Removing timer "' + name + '"');
         timers[name].stop();
         delete timers[name];
     }
+    
+    console.log(timers);
 }
 
 jQuery.event.special.frame = {
-    setup: function(data, namespaces) {
+    // Fires the first time an event is bound per element
+    setup: function(data, namespaces) {},
+    // Fires every time event is bound
+    add: function(handler, data, namespaces){
+        console.log('ADD');
+        
         var l = namespaces.length;
         
         // No namespaces, so spork master timer
@@ -111,12 +134,15 @@ jQuery.event.special.frame = {
             return;
         }
         
-        // Namespaces, so spork new timer for each namespace
+        // Namespaces exist, so spork new timer for each namespace
         while (l--) {
             spork( this, namespaces[l], data && data.frameDuration );
         }
     },
-    teardown: function(namespaces) {
+    // Fires every time event is unbound
+    remove: function(namespaces){
+        console.log('REMOVE');
+        
         var l = namespaces.length;
         
         // No namespaces, so unspork from all timers
@@ -127,11 +153,13 @@ jQuery.event.special.frame = {
             return;
         }
         
-        // Namespaces, so unspork timer for each name
+        // Namespaces exist, so unspork timer for each name
         while (l--) {
             unspork( this, namespaces[l] );
         }
     },
+    // Fires last time event is unbound per element
+    teardown: function(namespaces) {},
     handler: function(event){
         // let jQuery handle the calling of event handlers
         jQuery.event.handle.apply(this, arguments);
